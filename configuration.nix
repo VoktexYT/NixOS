18c43a0a4b41c8ec08df770a757db6fe60d2a761
VOKTEX <74386536+VoktexYT@users.noreply.github.com>
2025-11-30 14:05:51 -0500

    Format configuration.nix for consistency

--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-     (builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz")
-  ];
+  imports = [
+    ./hardware-configuration.nix
+    (builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz")
+  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager.enable = false;
-  virtualisation.docker.enable = true;
-  services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.enable = false;
-  users.users.ton_user = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    shell = pkgs.fish; 
-  };
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.enable = false;
+  virtualisation.docker.enable = true;
   
-  programs.sway.enable = true;
-  programs.sway.wrapperFeatures.gtk = true;
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-    firefox
-    qutebrowser
-  ];
+  services.xserver.enable = true; 
+  services.xserver.displayManager.lightdm.enable = false;
+  
+  users.users.ton_user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+    shell = pkgs.fish; 
+  };
+  
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+  ];
 
-  home-manager.users.ton_user = import ./home.nix;
-  system.stateVersion = "25.11";
+  home-manager.users.ton_user = import ./home.nix;
+  system.stateVersion = "25.11";
 }
